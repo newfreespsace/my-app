cd739ca573ba61d42ef0a754c92650cef0311412
lzy MacBook M2 <li_yii@163.com>
2026-02-03 18:51:03 +0800

    update

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import Calculator from '@/components/Calculator';
 export default async function Page() {
   return (
     <div className='p-10'>
-      <h1 className='text-2xl mb-4'>服务端计算示例</h1>
+      <h1 className='text-2xl mb-4'>服务端计算示例22</h1>
       <Calculator />
     </div>
   );
