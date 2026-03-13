33d9c71401f9d622206f59f0a59e75ea30653af5
lee-pc-wsl <zt4lxer@gmail.com>
2026-03-13 21:21:12 +0800

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,8 +1,19 @@
 import { NextRequest } from 'next/server';
+import Message from '@/models/Message';
+import dbConnect from '@/lib/db';
 
-export function GET(request: NextRequest) {
-  console.log(request);
+export async function GET(request: NextRequest) {
+  let time = new Date().toLocaleString('zh-CN', {
+    timeZone: 'Asia/Shanghai',
+  });
+  await dbConnect();
+  await Message.create({
+    message: time,
+  });
+  time = new Date().toLocaleString('zh-CN', {
+    timeZone: 'Asia/Shanghai',
+  });
   return Response.json({
-    message: 'get message from cro-job',
+    message: 'get message from server at ' + time,
   });
 }
---
+++ models/Message.ts
@@ -0,0 +1,11 @@
+import mongoose, { Model, InferSchemaType } from 'mongoose';
+
+const MessageSchema = new mongoose.Schema({
+  message: String,
+});
+
+export type IMessage = InferSchemaType<typeof MessageSchema> & { _id: mongoose.Types.ObjectId };
+
+const Message: Model<IMessage> = mongoose.models.Message || mongoose.model<IMessage>('Message', MessageSchema);
+
+export default Message;
