f2c681b39b94a2a02c3208a724155ac5397be8ba
lee-pc-wsl <zt4lxer@gmail.com>
2026-03-14 23:55:19 +0800

    1

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,11 @@
 import Footer from '@/components/Footer';
 
 const Page = () => {
-  return <Footer />;
+  return (
+    <div className="max-w-300 mx-auto">
+      <Footer />
+    </div>
+  );
 };
 
 export default Page;
